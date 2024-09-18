88dda61d50e66f99ef12bfd652adb9fd64b0e59d
thang <leviethoangthang1234@gmail.com>
2024-09-18 14:22:46 +0700

    1

--- THANG/Bai1.java
+++ THANG/Bai1.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class Bai1 {
     private String name;
-    public void nhap(){
+
+    public void nhap() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhap vao Ten ");
         this.name = sc.nextLine();
 
     }
-    public void in (){
+
+    public void in() {
         System.out.println(this.name);
     }
+
+    private String ten;
 }
