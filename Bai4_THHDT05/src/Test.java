98c2d113bc541673b32ac562ef82aa1ec78fcb5a
NguyenTuan180505 <hung0935347609@gmail.com>
2024-10-08 23:43:24 +0700

    Delete Bai4_THHDT05 directory

--- Bai4_THHDT05/src/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-    public static void main(String[] args) {
-        LaiSuat ls = new LaiSuat(0, 0, 0);
-        ls.Nhap();
-        System.out.println("Tien Suat la "+ls.TienLai());
-        System.out.println("Tien Goc Cuoi Ki la "+ls.TienGocCuoiKi());
-    }
-}
